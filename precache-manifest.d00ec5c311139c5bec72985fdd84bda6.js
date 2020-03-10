6951b3d329d5b2f1c17c8707756ee5ccba7cc86e
Fernando Lopez <lopezfer@globalhitss.com>
2020-03-10 17:42:51 -0300

    Updates

---
+++ precache-manifest.d00ec5c311139c5bec72985fdd84bda6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b113c79fed603983245b0e1b79063b54",
+    "url": "/mapa-covid-argentina/index.html"
+  },
+  {
+    "revision": "8b4d328dc8d4176cfb64",
+    "url": "/mapa-covid-argentina/static/js/2.2427022c.chunk.js"
+  },
+  {
+    "revision": "74922478e83fdf75dfe9f4d488e92d57",
+    "url": "/mapa-covid-argentina/static/js/2.2427022c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4cce5536eea8425a426",
+    "url": "/mapa-covid-argentina/static/js/main.5dc8d001.chunk.js"
+  },
+  {
+    "revision": "4145a50cc322c9373b5b",
+    "url": "/mapa-covid-argentina/static/js/runtime-main.1808c5dc.js"
+  }
+]);
