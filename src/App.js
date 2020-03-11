0aa6bbe09c8c80686a8f5c8faaa7203b2be887d9
Fernando Lopez <lopezfer@globalhitss.com>
2020-03-11 15:19:02 -0300

    actualizado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 import Moves from './components/Map'
 
@@ -15,6 +17,16 @@ const useStyles = makeStyles(theme => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  paper_header: {
+    padding: theme.spacing(2),
+    //textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  alerta: {
+    marginTop: theme.spacing(2),
+    color: "#fff", 
+    fontWeight: 500
+  }
 }));
 
 export default function App() {
@@ -22,10 +34,18 @@ export default function App() {
     return (
       <div className={classes.root}>
         <Grid container spacing={2}>
-          <Grid item xs={12}>
+          <Grid item xs={12} sm={6}>
             <Paper className={classes.paper} style={{backgroundColor: "#222", color: "#fff"}}>
               <h1>CORONAVIRUS ARGENTINA</h1>
             </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <MuiAlert severity="error" elevation={6} variant="filled">
+              LA OMS DECLARÓ EL CORONAVIRUS COMO PANDEMIA
+            </MuiAlert>
+            <MuiAlert severity="info" elevation={6} variant="filled" className={classes.alerta}>
+             El Gobierno ultima los detalles del decreto que impondrá la cuarentena obligatoria
+            </MuiAlert>
           </Grid>
           <Grid item xs={12} sm={4}>
             {/* ----- */}
