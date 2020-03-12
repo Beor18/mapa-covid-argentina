f5b209552c2aa2fd7ceb9ba38443e8e876f5f84b
Fernando Lopez <lopezfer@globalhitss.com>
2020-03-12 11:23:02 -0300

    actualizado

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ export default function App() {
               </Grid>
               <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                 <Paper className={classes.paper} style={{fontSize: "1.06em", backgroundColor: "#222", color: "rgb(230, 0, 0)", fontWeight: "bold"}}>
-                  <h2>TOTAL MUNDO <br></br> 126.670</h2>
+                  <h2>TOTAL MUNDO <br></br> 127.863</h2>
                 </Paper>
               </Grid>
             </Grid>
