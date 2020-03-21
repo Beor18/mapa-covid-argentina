25e853379fd106920616efa0e11f7c4cec730269
Fernando Lopez <lopezfer@globalhitss.com>
2020-03-21 12:48:01 -0300

    actualizado

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MuiAlert from '@material-ui/lab/Alert';
 // import Axios from "axios";
 import socketIOClient from "socket.io-client";
 
+import Tabla from './components/Tabla'
 import Map from './components/Map'
 
 const useStyles = makeStyles(theme => ({
@@ -113,6 +114,9 @@ export default function App() {
           </Grid>
           <Grid item xs={12} sm={12} md={8} lg={8} xl={8} className={classes.mapa}>
             <Map markerPosition={markerPosition}> </Map>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Tabla />
           </Grid>
         </Grid>
         </div>    
---
+++ src/components/Tabla.jsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+
+const columns = [
+  { id: 'provincia', label: 'Provincia', minWidth: 150 },
+//   { id: 'municipalidad', label: 'Municipalidad', minWidth: 150 },
+//   { id: 'barrio_ciudad', label: 'Ciudad / Barrio', minWidth: 150 },
+  {
+    id: 'confirmados',
+    label: 'Confirmados',
+    minWidth: 150,
+    align: 'center'
+  },
+  {
+    id: 'fallecidos',
+    label: 'Fallecidos',
+    minWidth: 150,
+    align: 'center'
+  },
+//   {
+//     id: 'recuperados',
+//     label: 'Recuperados',
+//     minWidth: 170,
+//     align: 'center'
+//   },
+];
+
+function createData(provincia, municipalidad, barrio_ciudad, confirmados, fallecidos, recuperados) {
+  return { provincia, municipalidad, barrio_ciudad, confirmados, fallecidos, recuperados };
+}
+
+const rows = [
+  createData('CABA', '', '', 65, 2, 25),
+  createData('Buenos Aires', '', '', 44, 1, 1),
+  createData('Chaco', '', '', 16, 1, 0),
+  createData('Córdoba', '', '', 13, 0, 0),
+  createData('Tierra del Fuego', '', '', 3, 0, 0),
+  createData('Entre Ríos', '', '', 3, 0, 0),
+  createData('Río Negro', '', '', 3, 0, 1),
+  createData('Neuquén', '', '', 2, 0, 0),
+  createData('Santa Fe', '', '', 2, 0, 0),
+  createData('San Luis', '', '', 1, 0, 0),
+  createData('Santiago del Estero', '', '', 1, 0, 0),
+  createData('Corrientes', '', '', 1, 0, 0),
+  createData('Tucumán', '', '', 1, 0, 0),
+  createData('Santa Cruz', '', '', 1, 0, 0),
+  createData('Salta', '', '', 1, 0, 0),
+  createData('Jujuy', '', '', 1, 0, 0),
+];
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 440,
+  },
+//   table_head: {
+//       backgroundColor: 'rgb(34, 34, 34)',
+//       color: '#fff'
+//   }
+});
+
+export default function Tabla() {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                  className={classes.table_head}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  {columns.map(column => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
