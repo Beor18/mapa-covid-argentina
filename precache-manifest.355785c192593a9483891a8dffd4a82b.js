0bae4e9a7d20312c9d703d74821f5aceae896076
Fernando Lopez <lopezfer@globalhitss.com>
2020-03-21 15:18:09 -0300

    Updates

--- precache-manifest.355785c192593a9483891a8dffd4a82b.js
+++ precache-manifest.355785c192593a9483891a8dffd4a82b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "61917c79291ae6fbc6c658d090f4e24e",
+    "revision": "4c80e3ccc38b3d9a1ed8f78f0c20386b",
     "url": "/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.e0ac8622.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "49141a7b60d9b6acf2ac",
-    "url": "/static/js/main.09bcea12.chunk.js"
+    "revision": "a0fe0a3002c6a7e94274",
+    "url": "/static/js/main.c82897b7.chunk.js"
   },
   {
     "revision": "870af51370a3c7c21a9e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48e036d8defe2e7b58f144a56bb60579.js"
+  "/precache-manifest.355785c192593a9483891a8dffd4a82b.js"
 );
 
 self.addEventListener('message', (event) => {
